bd642eccb7ba41da329c76abaf2337b975f60765
Samuel Falcon <saxclar1025@gmail.com>
2018-04-21 11:28:59 -0400

    added post method to server

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+const maxTables = 5;
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+var reservations = [];
+var waitlist = [];
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,9 +23,18 @@ app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
   });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "make.html"));
-  });
+app.get("/reserve", function(req, res) {
+res.sendFile(path.join(__dirname, "make.html"));
+});
+
+app.post("/api/tables", (req, res)=>{
+    if(reservations.length < maxTables) {
+        reservations.push(req.body);
+        return res.end(true);
+    }
+    waitlist.push(req.body);
+    return res.end(false);
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
