0aa945148133c8d4ca67d9b17fc6c2e7c5103392
Josh <josh@Joshs-MacBook-Pro.local>
2018-04-21 11:06:30 -0400

    express calls

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "make.html"));
+  });
+
